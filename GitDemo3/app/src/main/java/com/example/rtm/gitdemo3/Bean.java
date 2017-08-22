43a010280769e74d5d17b61f86a1a6d7c7f1a57e
RuiTingM <302854619@>
2017-08-22 09:54:23 +0800

    数据类增加了名字和年龄的属性

--- GitDemo3/app/src/main/java/com/example/rtm/gitdemo3/Bean.java
+++ GitDemo3/app/src/main/java/com/example/rtm/gitdemo3/Bean.java
@@ -6,4 +6,21 @@ package com.example.rtm.gitdemo3;
 
 public class Bean {
     // 只是一个数据类
+    String name, age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
